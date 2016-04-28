8fe8c43819874ce49f1e1a4c3f575b70530cd294
brendanbailey2015 <brendanbailey2015@yahoo.com>
2016-04-27 21:19:20 -0400

    Small Google-Related changes

--- app/src/main/java/edu/gvsu/cis/headyn/budgetapplaptop/fragments/SettingsFragment.java
+++ app/src/main/java/edu/gvsu/cis/headyn/budgetapplaptop/fragments/SettingsFragment.java
@@ -1,11 +1,28 @@
 package edu.gvsu.cis.headyn.budgetapplaptop.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import edu.gvsu.cis.headyn.budgetapplaptop.R;
 
@@ -15,14 +32,54 @@ import edu.gvsu.cis.headyn.budgetapplaptop.R;
 public class SettingsFragment extends Fragment {
 
     public SettingsFragment(){}
+   // private Button googleButton;
+    private GoogleApiClient mGoogleApiClient;
+    private Context mContext;
+
+    public void onAttach(final Activity activity) {
+        super.onAttach(activity);
+        mContext = activity;
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View myView = inflater.inflate(R.layout.fragment_settings, container, false);
+        View myView =  inflater.inflate(R.layout.fragment_settings, container, false);
+       // this.googleButton = (Button) myView.findViewById(R.id.googleButton);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
 
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(mContext )
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
+        // Customize sign-in button. The sign-in button can be displayed in
+        // multiple sizes and color schemes. It can also be contextually
+        // rendered based on the requested scopes. For example. a red button may
+        // be displayed when Google+ scopes are requested, but a white button
+        // may be displayed when only basic profile is requested. Try adding the
+        // Scopes.PLUS_LOGIN scope to the GoogleSignInOptions to see the
+        // difference.
+
+        //SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+       // signInButton.setSize(SignInButton.SIZE_STANDARD);
+       // signInButton.setScopes(gso.getScopeArray());
 
         return myView;
     }
-}
+
+
+
+
+
+        }
+
+
+
